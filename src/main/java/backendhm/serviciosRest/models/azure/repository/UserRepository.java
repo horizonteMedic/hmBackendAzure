5cf8e2714e394514b630a2e7a017729b966a75ad
horizonteMedic <sistemas@horizontemedic.com>
2024-05-04 21:50:48 -0500

    posible solucion

--- src/main/java/backendhm/serviciosRest/models/azure/repository/UserRepository.java
+++ src/main/java/backendhm/serviciosRest/models/azure/repository/UserRepository.java
@@ -3,10 +3,12 @@ package backendhm.serviciosRest.models.azure.repository;
 import backendhm.serviciosRest.models.azure.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<Usuario,Long> {
    // @Query(value = "SELECT * FROM users WHERE username=:nombre;", nativeQuery=true)
     Optional<Usuario> findByUsername(String username);
