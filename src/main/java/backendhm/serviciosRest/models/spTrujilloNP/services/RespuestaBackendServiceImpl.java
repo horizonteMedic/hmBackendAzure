44f2c7a673e416b45708cbc908b6b8ddc3aa4e3a
horizonteMedic <sistemas@horizontemedic.com>
2024-05-22 15:32:09 -0500

    transaction

--- src/main/java/backendhm/serviciosRest/models/spTrujilloNP/services/RespuestaBackendServiceImpl.java
+++ src/main/java/backendhm/serviciosRest/models/spTrujilloNP/services/RespuestaBackendServiceImpl.java
@@ -8,7 +8,9 @@ import backendhm.serviciosRest.models.spTrujilloNP.entity.RespuestaBackendNP;
 import backendhm.serviciosRest.models.spTrujilloNP.repository.IRespuestaBackendNPRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional("trujilloNPTransactionManagerFactory")
 @Service
 public class RespuestaBackendServiceImpl implements IRespuestaBackendService{
 
