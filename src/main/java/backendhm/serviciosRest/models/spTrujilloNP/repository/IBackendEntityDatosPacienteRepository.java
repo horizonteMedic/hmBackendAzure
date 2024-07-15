40fdffecb745c1ba29e2a8663675fff7e663c2e6
horizonteMedic <sistemas@horizontemedic.com>
2024-07-15 13:46:29 -0500

    parche en datos de paciente busqueda

--- src/main/java/backendhm/serviciosRest/models/spTrujilloNP/repository/IBackendEntityDatosPacienteRepository.java
+++ src/main/java/backendhm/serviciosRest/models/spTrujilloNP/repository/IBackendEntityDatosPacienteRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 @Repository
 public interface IBackendEntityDatosPacienteRepository extends JpaRepository<BackendEntityDatosPaciente,Long> {
     @Query(value = "select cod_pa,nombres_pa,CAST(fecha_nacimiento_pa as text),CAST (sexo_pa AS TEXT),email_pa,lugar_nac_pa, nivel_est_pa, ocupacion_pa, estado_civil_pa, direccion_pa,departamento_pa, provincia_pa,\n" +
-            "\tdistrito_pa,caserio_pa, cast(foto_pa as text), cod_aleatorio_pa,tel_casa_pa, tel_trabajo_pa, cel_pa, CAST(fecha_registro_pa as text), apellidos_pa, CAST(hora_registro_pa as text), tipo_doc\n" +
-            "  from datos_paciente where cod_pa=?", nativeQuery=true)
+            "           distrito_pa,caserio_pa, cast(foto_pa as text), CASE  WHEN cod_aleatorio_pa IS NULL THEN 0 ELSE cod_aleatorio_pa END ,tel_casa_pa, tel_trabajo_pa, cel_pa, CAST(fecha_registro_pa as text), apellidos_pa, CAST(hora_registro_pa as text), tipo_doc\n" +
+            "            from datos_paciente where cod_pa=?", nativeQuery=true)
     Optional<BackendEntityDatosPaciente> busquedaDatosPacienteDNI(long dni);
 }
